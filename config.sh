1c719cc134c70b7b30c2f6f9cc087be1abab5979
Martin Jindra <43207169+MartinJindra@users.noreply.github.com>
2020-07-09 23:15:58 +0200

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
+hwclock --systohc
+echo "Please enter name for computer: "
+read pcname
+echo $pcname > /etc/hostname;
+echo "KEYMAP=de-latin1" > /etc/vconsole.conf
+echo "LANG=de_AT.UTF-8" > /etc/locale.conf
+echo "127.0.0.1		localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1		lxUSB.localdomain  lxUSB" >> /etc/hosts
+echo "de_AT.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "Password for root:"
+passwd
+echo "New User:"
+read username
+useradd $username -m -G users -s /bin/fish
+passwd $username
+chmod +w /etc/sudoers
+echo "$username ALL=(ALL) ALL" > /etc/sudoers
+chmod -w /etc/sudoers
+usermod root -s /bin/fish
+grub-install --target=x86_64-efi --efi-directory=/efi --removable
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable sddm
+systemctl enable NetworkManager
+systemctl enable bluetooth
+
+pip3 install protonvpn-cli
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+lsblk
+echo "Drive:"
+read drive
+cfdisk $drive
+mkfs.fat -F32 "$drive"1
+mkfs.ext4 -O "^has_journal" "$drive"2
+mkdir /mnt/root
+mount "$drive"2 /mnt/root
+pacstrap -c /mnt/root linux linux-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode firefox chromium xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl fish bash-completion python-pip python npm nodejs bluez fatresize jfsutils lsof wget arandr openvpn dialog python-pip python-setuptools neofetch arch-install-scripts
+mkdir /mnt/root/efi
+mount "$drive"1 /mnt/root/efi
+genfstab -U /mnt/root/ >> /mnt/root/etc/fstab
+vim /mnt/root/etc/fstab
