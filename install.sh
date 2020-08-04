44aa1cfbb00021593fe8e0e01c4561c2a55acbb6
mjindra2-tgm <mjindra2@student.tgm.ac.at>
2020-08-04 22:50:34 +0200

    added -p flag to mkdir

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ read drive
 cfdisk $drive
 mkfs.fat -F32 "$drive"1
 mkfs.ext4 -O "^has_journal" "$drive"2
-mkdir /mnt/root
+mkdir -p /mnt/root
 mount "$drive"2 /mnt/root
 pacstrap -c /mnt/root linux linux-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr openvpn dialog python-setuptools neofetch arch-install-scripts tar xz bzip2 gzip zstd
-mkdir /mnt/root/efi
+mkdir -p /mnt/root/efi
 mount "$drive"1 /mnt/root/efi
 genfstab -U /mnt/root/ >> /mnt/root/etc/fstab
 vim /mnt/root/etc/fstab
