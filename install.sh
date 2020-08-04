21625cbfc8a77f60b8b4092ee4a32cb8cc18968b
mjindra2-tgm <mjindra2@student.tgm.ac.at>
2020-08-04 22:49:01 +0200

    removed GNU Compiler from package list

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkfs.fat -F32 "$drive"1
 mkfs.ext4 -O "^has_journal" "$drive"2
 mkdir /mnt/root
 mount "$drive"2 /mnt/root
-pacstrap -c /mnt/root linux linux-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr openvpn dialog python-setuptools neofetch arch-install-scripts gcc tar xz bzip2 gzip zstd
+pacstrap -c /mnt/root linux linux-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr openvpn dialog python-setuptools neofetch arch-install-scripts tar xz bzip2 gzip zstd
 mkdir /mnt/root/efi
 mount "$drive"1 /mnt/root/efi
 genfstab -U /mnt/root/ >> /mnt/root/etc/fstab
