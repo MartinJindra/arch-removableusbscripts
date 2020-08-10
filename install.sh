6997bbcf233666c9c143a36b8207c4af3f67b46b
mjindra2-tgm <mjindra2@student.tgm.ac.at>
2020-08-10 23:08:19 +0200

    script will differentiate between manjaro and arch

--- install.sh
+++ install.sh
@@ -15,7 +15,14 @@ mkfs.ext4 -O "^has_journal" "$drive"2
 mkdir -p /mnt/root
 mount "$drive"2 /mnt/root
 # install packages to disk
-pacstrap -c /mnt/root linux linux-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode gnuplot wxmaxima konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl zsh fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr openvpn dialog python-setuptools neofetch arch-install-scripts tar xz bzip2 gzip zstd speedtest-cli
+release=$(head -n 1 /etc/os-release)
+
+if [[ $release == 'NAME="Arch Linux"' ]]; then
+	pacstrap -c /mnt/root linux linux-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode gnuplot wxmaxima konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl zsh fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr openvpn dialog python-setuptools neofetch arch-install-scripts tar xz bzip2 gzip zstd speedtest-cli
+elif [[ $release = 'NAME="Manjaro Linux"' ]]; then 
+	pacstrap -c /mnt/root linux-latest linux-latest-headers linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode gnuplot wxmaxima konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl zsh fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr openvpn dialog python-setuptools neofetch arch-install-scripts tar xz bzip2 gzip zstd speedtest-cli
+fi
+
 # mount boot partition to system
 mkdir -p /mnt/root/efi
 mount "$drive"1 /mnt/root/efi
