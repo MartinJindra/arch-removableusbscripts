14075a5b1ea0cac8b549e532c3e79e20036ba6a3
Martin Jindra <jindra.martin@hotmail.com>
2021-03-07 19:08:20 +0100

    reading input is now on same line

--- config.sh
+++ config.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
+
 # set timezone
 ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
 hwclock --systohc
+
 # set system name
-echo "Please enter name for computer: "
-read pcname
+read -rp 'Please enter name for computer: ' pcname
 echo $pcname > /etc/hostname;
+
 # set keyboard layout
 echo "KEYMAP=de-latin1" > /etc/vconsole.conf
+
 # set language 
 echo "LANG=de_AT.UTF-8" > /etc/locale.conf
+
 # set network configuration
 echo "127.0.0.1		localhost" >> /etc/hosts
 echo "::1		localhost" >> /etc/hosts
 echo "127.0.1.1		$pcname.localdomain  $pcname" >> /etc/hosts
+
 # generate locale 
 echo "de_AT.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
+
 # set password for root
 echo "Password for root:"
 passwd
 cp .bashrc /root/
+
 # set new user
-echo "New User:"
-read username
+read -rp 'New User: ' username
 useradd $username -m -G users -s $(which zsh)
 passwd $username
+
 # set permission for new user for sudo
 chmod +w /etc/sudoers
 echo "$username ALL=(ALL) ALL" >> /etc/sudoers
 chmod -w /etc/sudoers
+
 # copy shell scripts to home dir
 usermod root -s /bin/zsh
 cp .zshrc "/home/$username/"
 cp .bashrc "/home/$userame/"
+
 # install grub
 grub-install --target=x86_64-efi --efi-directory=/efi --removable
 grub-mkconfig -o /boot/grub/grub.cfg
+
 # enable display manager, networkmanager and bluetooth
 systemctl enable sddm
 systemctl enable NetworkManager
 systemctl enable bluetooth
-# install instaloader
-pip3 install instaloader
+
--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+
 # unmount drive if script is started again
 umount -R /mnt/root
+
 # install install-scripts
-pacman -Sy --needed arch-install-scripts
+pacman -Sy --needed --noconfirm arch-install-scripts
+
 # choose disk and format it
 sync
 lsblk
-echo "Drive:"
-read drive
+read -rp 'Drive ' drive
 cfdisk $drive
 mkfs.fat -F32 "$drive"1
 mkfs.ext4 -O "^has_journal" "$drive"2
+
 # mount root partition to system
 mkdir -p /mnt/root
 mount "$drive"2 /mnt/root
+
 # install packages to disk
 release=$(head -n 1 /etc/os-release)
 packages=(linux-firmware base base-devel grub efibootmgr networkmanager exfat-utils mtools ntfs-3g amd-ucode intel-ucode gnuplot wxmaxima konqueror xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau kitty lxqt sddm kate ktorrent kcalc mpv kpatience ksysguard ksystemlog veracrypt git vim nano partitionmanager bashtop htop openssh openssl sqlmap nmap arp-scan youtube-dl zsh zsh-syntax-highlighting zsh-autosuggestions zsh-completions fish bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr dialog python-setuptools neofetch arch-install-scripts tar xz bzip2 gzip zstd speedtest-cli)
@@ -22,12 +26,15 @@ if [[ $release == 'NAME="Arch Linux"' ]]; then
 elif [[ $release = 'NAME="Manjaro Linux"' ]]; then 
 	pacstrap -c /mnt/root linux-latest linux-latest-headers $packages
 fi
+
 # mount boot partition to system
 mkdir -p /mnt/root/efi
 mount "$drive"1 /mnt/root/efi
+
 # generate fstab
 genfstab -U /mnt/root/ >> /mnt/root/etc/fstab
 vim /mnt/root/etc/fstab
+
 # copy .zshrc and the config file to disk 
 cp config.sh /mnt/root/root
 cp .zshrc /mnt/root/root
