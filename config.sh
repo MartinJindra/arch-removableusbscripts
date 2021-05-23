2fbff0bbc31ca7cb97c212c8b5b50a5722bae462
Martin Jindra <mjindra@derchef.email>
2021-05-24 00:06:55 +0200

    added lightdm greeter config

--- config.sh
+++ config.sh
@@ -24,6 +24,10 @@ echo "de_AT.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
+# add light gtk greeter to lightdm config
+lightdm_file=$(sed '/\[LightDM\]/a greeter-session=lightdm-gtk-greeter' /etc/lightdm/lightdm.conf)
+echo $lightdm_file > /etc/lightdm/lightdm.conf
+
 # set password for root
 echo "Password for root:"
 passwd root
