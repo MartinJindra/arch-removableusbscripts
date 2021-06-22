031cfd8d17a51b13b1fc3af738066c510ff81c0e
MartinJindra <mjindra@derchef.email>
2021-06-22 23:47:38 +0200

    installscripts warns when not executed as root and added extra packages

--- install.sh
+++ install.sh
@@ -1,53 +1,59 @@
 #!/bin/bash
 
-mountpoint="/mnt/root"
-
-# unmount drive if script is started again
-umount -R $mountpoint 
-
-# install install-scripts
-pacman -Sy --needed --noconfirm arch-install-scripts dosfstools
-
-# choose disk and format it
-sync
-lsblk
-read -rp 'Name of the drive: ' drive
-
-if [[ $(mount | grep -c "$drive") -eq 0 ]];
+if [[ $(/usr/bin/id -u) -ne 0 ]];
 then
-	cfdisk "$drive"
-	mkfs.fat -F32 "$drive"1
-	mkfs.ext4 -O "^has_journal" "$drive"2
+    echo "Not running as root"
+    exit
+else
+    mountpoint="/mnt/root"
 
-	# mount root partition to system
-	mkdir -p "$mountpoint"
-	mount "$drive"2 "$mountpoint"
-	
-	# install packages to disk
-	release=$(head -n 1 /etc/os-release)
-	packages=(linux-firmware base base-devel grub efibootmgr exfat-utils mtools ntfs-3g amd-ucode intel-ucode grub-customizer gnuplot wxmaxima qutebrowser midori gst-plugins-good gst-libav xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau kitty lxqt leafpad transmission-cli transmission-gtk galculator mpv kpat veracrypt git neovim nano gparted bpytop htop openssh openssl sqlmap nmap arp-scan youtube-dl fish zsh zsh-syntax-highlighting zsh-autosuggestions zsh-completions bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr neofetch arch-install-scripts tar xz bzip2 gzip zstd speedtest-cli)
-    if [[ "$release" == 'NAME="Arch Linux"' ]]; 
-	then
-		pacstrap -c "$mountpoint" linux-lts linux-lts-headers linux-lts-docs networkmanager sddm "${packages[@]}"
-	elif [[ "$release" = 'NAME="Artix Linux"' ]]; 
-	then 
-		pacstrap -c $mountpoint linux-lts linux-lts-headers linux-lts-docs openrc elogind-openrc networkmanager-openrc sddm-openrc artix-archlinux-support "${packages[@]}"
-    elif [[ "$release" = 'NAME="Manjaro Linux"' ]]; 
-	then 
-		pacstrap -c "$mountpoint" linux510-lts linux510-headers linux510-docs networkmanager sddm "${packages[@]}"
-	fi
+    # unmount drive if script is started again
+    umount -R $mountpoint 
 
-	# mount boot partition to system
-	mkdir -p "$mountpoint"/efi
-	mount "$drive"1 "$mountpoint"/efi
+    # install install-scripts
+    pacman -Sy --needed --noconfirm arch-install-scripts dosfstools
+
+    # choose disk and format it
+    sync
+    lsblk
+    read -rp 'Name of the drive: ' drive
+
+    if [[ $(mount | grep -c "$drive") -eq 0 ]];
+    then
+	    cfdisk "$drive"
+	    mkfs.fat -F32 "$drive"1
+	    mkfs.ext4 -O "^has_journal" "$drive"2
+
+	    # mount root partition to system
+	    mkdir -p "$mountpoint"
+	    mount "$drive"2 "$mountpoint"
 	
-	# generate fstab
-	chmod +w "$mountpoint"/etc/fstab
-    genfstab -U "$mountpoint" >> "$mountpoint"/etc/fstab
-	nvim "$mountpoint"/etc/fstab
+	    # install packages to disk
+	    release=$(head -n 1 /etc/os-release)
+	    packages=(linux-firmware base base-devel grub efibootmgr exfat-utils mtools ntfs-3g amd-ucode intel-ucode grub-customizer meld gnome-disk-utility gnuplot wxmaxima qutebrowser midori gst-plugins-good gst-libav xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau kitty lxqt leafpad transmission-cli transmission-gtk galculator mpv kpat veracrypt git neovim nano gparted bpytop htop openssh openssl sqlmap nmap arp-scan youtube-dl fish zsh zsh-syntax-highlighting zsh-autosuggestions zsh-completions bash-completion python-pip python bluez fatresize jfsutils lsof wget arandr neofetch arch-install-scripts tar xz bzip2 gzip zstd speedtest-cli)
+        if [[ "$release" == 'NAME="Arch Linux"' ]]; 
+	    then
+		    pacstrap -c "$mountpoint" linux-lts linux-lts-headers linux-lts-docs networkmanager sddm "${packages[@]}"
+	    elif [[ "$release" = 'NAME="Artix Linux"' ]]; 
+	    then 
+		    pacstrap -c $mountpoint linux-lts linux-lts-headers linux-lts-docs openrc elogind-openrc networkmanager-openrc sddm-openrc artix-archlinux-support "${packages[@]}"
+        elif [[ "$release" = 'NAME="Manjaro Linux"' ]]; 
+	    then 
+		    pacstrap -c "$mountpoint" linux510-lts linux510-headers linux510-docs networkmanager sddm "${packages[@]}"
+	    fi
+
+	    # mount boot partition to system
+	    mkdir -p "$mountpoint"/efi
+	    mount "$drive"1 "$mountpoint"/efi
 	
-	# copy .zshrc and the config file to disk 
-	cp config.sh "$mountpoint"/root
-	cp --force .zshrc "$mountpoint"/root
-	cp --force .bashrc "$mountpoint"/root
+	    # generate fstab
+	    chmod +w "$mountpoint"/etc/fstab
+        genfstab -U "$mountpoint" >> "$mountpoint"/etc/fstab
+	    nvim "$mountpoint"/etc/fstab
+	
+	    # copy .zshrc and the config file to disk 
+	    cp config.sh "$mountpoint"/root
+	    cp --force .zshrc "$mountpoint"/root
+	    cp --force .bashrc "$mountpoint"/root
+    fi
 fi
